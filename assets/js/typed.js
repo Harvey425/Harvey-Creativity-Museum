9228ce7944c5fdbd5c33a589c0164e5715b18244
Harvey425 <1371906748@qq.com>
2024-05-11 04:09:56 +0800

    new

---
+++ assets/js/typed.js
@@ -0,0 +1,8 @@
+ /***************Text Typing Effect***************/
+
+    var typing = new Typed(".text", {
+      strings: ["数据管理-新媒体双学位修读者","摄影发烧友","影像创作者", "视觉设计爱好者", "交互设计探索者", "数据可视化达人"],
+      typeSpeed: 100,
+      backSpeed: 40,
+      loop: true,
+    });
